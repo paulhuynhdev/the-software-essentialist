76fc6fc36b814d63e4eb4ce0f764b3509c44c3bf
Huynh Hoan Vu <vuhh5@fpt.com>
2024-08-25 15:46:21 +0700

    feat: get ran---

---
+++ ThePhasesOfCraftship/2_best_practice_first/strategicDesignPart2/exercises/1_CharacterizeAndRefactorE2EBackend/begin/shared/getRandomPort.ts
@@ -0,0 +1,12 @@
+import * as net from "net";
+
+export function getRandomPort(): Promise<number> {
+  return new Promise((resolve, reject) => {
+    const server = net.createServer();
+    server.listen(0, () => {
+      const port = (server.address() as net.AddressInfo).port;
+      server.close(() => resolve(port));
+    });
+    server.on("error", reject);
+  });
+}
