3fde234fab3a0d3f65a1cc9b1388d1736a6bd6cc
Huynh Hoan Vu <vuhh5@fpt.com>
2025-01-23 22:44:12 +0700

    refactor: reduce duplication

--- ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
+++ ThePhasesOfCraftship/2_best_practice_first/testingBasics/exercises/3_Manage_Complexity/3_2_Boolean_Calculator/src/index.spec.ts
@@ -1,25 +1,27 @@
-import { describe, it, expect } from "@jest/globals";
+import { describe, it, expect, beforeEach } from "@jest/globals";
 import { BooleanCalculator } from "./index";
 
 describe("boolean calculator", () => {
+  let booleanCalculator: BooleanCalculator;
+
+  beforeEach(() => {
+    booleanCalculator = new BooleanCalculator();
+  });
+
   describe("single values", () => {
     it('should return true for "TRUE"', () => {
-      const booleanCalculator = new BooleanCalculator();
       expect(booleanCalculator.evaluate("TRUE")).toBe(true);
     });
     it('should return false for "FALSE"', () => {
-      const booleanCalculator = new BooleanCalculator();
       expect(booleanCalculator.evaluate("FALSE")).toBe(false);
     });
   });
 
   describe("NOT operator", () => {
     it("should return true for NOT TRUE", () => {
-      const booleanCalculator = new BooleanCalculator();
       expect(booleanCalculator.evaluate("NOT TRUE")).toBe(false);
     });
     it("should return false for NOT FALSE", () => {
-      const booleanCalculator = new BooleanCalculator();
       expect(booleanCalculator.evaluate("NOT FALSE")).toBe(true);
     });
   });
